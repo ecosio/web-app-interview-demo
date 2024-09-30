0bedb0aecbfa3fc771e8583c01b8693e76ae71c3
Martin Kallinger <martin.kallinger@ecosio.com>
2024-09-30 14:44:10 +0200

    prettier

--- contacts-frontend/src/ApiContext.ts
+++ contacts-frontend/src/ApiContext.ts
@@ -2,19 +2,24 @@ import React from "react";
 import { Configuration, ContactControllerApi } from "./apiclient";
 
 export class ApiContextState {
-    public readonly contactApi: ContactControllerApi;
+  public readonly contactApi: ContactControllerApi;
 
-    public constructor(url:string, user: string | undefined, password: string | undefined) {
-        const config = new Configuration ({
-            basePath: url,
-            username: user,
-            password: password
-        });
+  public constructor(
+    url: string,
+    user: string | undefined,
+    password: string | undefined
+  ) {
+    const config = new Configuration({
+      basePath: url,
+      username: user,
+      password: password,
+    });
 
-        this.contactApi = new ContactControllerApi(config);
-
-    }
+    this.contactApi = new ContactControllerApi(config);
+  }
 }
 
-export const ApiContext = React.createContext<ApiContextState>(new ApiContextState("","",""));
+export const ApiContext = React.createContext<ApiContextState>(
+  new ApiContextState("", "", "")
+);
 export default ApiContext;
