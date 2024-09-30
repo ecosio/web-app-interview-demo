1324569c81f695e3ae8c831fea81e8719674470a
Robert Gründler <robert.gruendler@ecosio.com>
2024-09-30 08:23:08 +0200

    chore: remove a couple of lines of code to be added in MR again

--- contacts-backend/src/main/java/at/kallinger/contacts/api/ContactController.java
+++ contacts-backend/src/main/java/at/kallinger/contacts/api/ContactController.java
@@ -3,13 +3,11 @@ package at.kallinger.contacts.api;
 import at.kallinger.contacts.service.ContactService;
 
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contacts")
-@CrossOrigin(origins = "http://localhost:3000")
 @Validated
 public class ContactController {
 
